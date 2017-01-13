4ca65a9665435b4ff77c6adc612bb185fc155f8c
The-DarkBeast <pranaynarang@gmail.com>
2017-01-13 19:43:54 +0000

    governor: Add Dark_Stealth governor and did some cleanup
    Signed-off-by: The-DarkBeast <pranaynarang@gmail.com>
    Signed-off-by: As007Akash <akashpathange.aaa47@gmail.com>
    Signed-off-by: Nameless-Devil <suryaprakash.zna@gmail.com>

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -459,21 +459,9 @@ extern struct cpufreq_governor cpufreq_gov_lionheart;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_PEGASUSQ)
 extern struct cpufreq_governor cpufreq_gov_pegasusq;
 #define CPUFREQ_DEFAULT_GOVERNOR        (&cpufreq_gov_pegasusq)
-#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_IMPULSE)
-extern struct cpufreq_governor cpufreq_gov_impulse;
-#define CPUFREQ_DEFAULT_GOVERNOR       (&cpufreq_gov_impulse)
-#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_HYPER)
-extern struct cpufreq_governor cpufreq_gov_hyper;
-#define CPUFREQ_DEFAULT_GOVERNOR        (&cpufreq_gov_hyper)
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_NIGHTMARE)
 extern struct cpufreq_governor cpufreq_gov_nightmare;
 #define CPUFREQ_DEFAULT_GOVERNOR        (&cpufreq_gov_nightmare)
-#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_IRONACTIVE)
-extern struct cpufreq_governor cpufreq_gov_ironactive;
-#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_ironactive)
-#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_DARKNESS)
-extern struct cpufreq_governor cpufreq_gov_darkness;
-#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_darkness)
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_DARK_STEALTH)
 extern struct cpufreq_governor cpufreq_gov_dark_stealth;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_dark_stealth)
