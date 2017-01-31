b8bcbfaa74509269ba531429e5b54de90e324fc2
As007Akash <akashpathange.aaa47@gmail.com>
2017-01-31 19:55:37 +0530

    Fix Compiling: Remove dsb(); function in "arch/arm64/mm/mmu.c"
    Signed-off-by: As007Akash <akashpathange.aaa47@gmail.com>

--- arch/arm64/mm/mmu.c
+++ arch/arm64/mm/mmu.c
@@ -597,7 +597,6 @@ void __init paging_init(void)
 	empty_zero_page = virt_to_page(zero_page);
 
 	/* Ensure the zero page is visible to the page table walker */
-	dsb();
 
 	/*
 	 * TTBR0 is only used for the identity mapping at this stage. Make it
