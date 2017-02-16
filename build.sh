30324cfd97432f7d575d8fcedf172a8c92054fd0
Nameless-Devil <suryaprakash.zna@gmail.com>
2017-02-16 20:19:08 +0530

    TOOLCHAIN: changed toolchain to GOOGLETC
    Signed-off-by: Nameless-Devil <suryaprakash.zna@gmail.com>

--- build.sh
+++ build.sh
@@ -9,7 +9,7 @@ yellow='\033[0;33m'
 red='\033[0;31m'
 nocol='\033[0m'
 # Modify the following variable if you want to build
-export CROSS_COMPILE=~/android/toolchain/UBERTC/aarch64-linux-android-4.9/bin/aarch64-linux-android-
+export CROSS_COMPILE=~/android/toolchain/GOOGLETC/aarch64-linux-android-4.9.x/bin/aarch64-linux-android-
 export ARCH=arm64
 export SUBARCH=arm64
 export KBUILD_BUILD_USER=Devil91221
